c7a6c26be5f6d251268a8b5d8d85183969c9505f
nullJaX <nullJaX@users.noreply.github.com>
2018-11-26 21:38:54 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from skimage.segmentation import mark_boundaries
+from skimage import io
+import matplotlib.pyplot as plt
+
+from utils import obtain_superpixels, get_confidence_map, get_neighbors, average_rgb_for_superpixels, sort_values
+
+if __name__ == '__main__':
+    image = io.imread("lena.png")
+    segments = obtain_superpixels(image, 10, 5)
+    confidence_map = get_confidence_map(image, segments)
+    neighbors = get_neighbors(segments)
+    superpixels_vectors = average_rgb_for_superpixels(image, segments)
+    print(confidence_map)
+    print(neighbors)
+    print(superpixels_vectors)
+    print("==================================")
+    print(sort_values(superpixels_vectors, neighbors, confidence_map, "confidence"))
+    print("==================================")
+    print(sort_values(superpixels_vectors, neighbors, confidence_map, "bfs"))
+    print("==================================")
+    print(sort_values(superpixels_vectors, neighbors, confidence_map, "dfs"))
+
+    fig = plt.figure("Superpixels")
+    ax = fig.add_subplot(1, 1, 1)
+    ax.imshow(mark_boundaries(image, segments))
+    plt.axis("off")
+    plt.show()
